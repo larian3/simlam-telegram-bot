4e57d6ae2af22896ce7f7e5fa798008a5b626fa9
Lariane <larianelima22@gmail.com>
2025-11-10 10:33:32 -0300

    feat: melhorias no cabeçalho

--- simlam_scraper.py
+++ simlam_scraper.py
@@ -193,8 +193,15 @@ def buscar_processo(search_term, search_type="processo"):
         search_page_url = urljoin(base_url, search_page)
 
         session = requests.Session()
+        # Define um conjunto de cabeçalhos para simular um navegador real
         session.headers.update({
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'DNT': '1', # Do Not Track
         })
 
         # Define um timeout padrão para todas as requisições da sessão
